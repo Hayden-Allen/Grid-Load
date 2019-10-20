2e1dbd91604c82af1cff5e15405606cde77f65ee
Hayden Allen <41128713+Hayden-Allen@users.noreply.github.com>
2019-10-20 11:51:38 -0500

    Add files via upload

--- scripts/BitSet.js
+++ scripts/BitSet.js
@@ -1,18 +1,24 @@
-function BitSet(value) {
-	this.value = value || 0;
-	this.at = function(index){
-		return (this.value & Math.pow(2, index)) > 0;
+class BitSet{
+	constructor(value){	//default value is 0
+		this.value = value || 0;
 	}
-	this.set = function(index){
-		this.value |= Math.pow(2, index);
+	bit(index){	//only bit at index is 1
+		return 1 << index;
 	}
-	this.reset = function(index){
-		this.value &= ~(Math.pow(2, index));
+	at(index){	//true if bit at index is 1, false if it is 0
+		return (this.value & this.bit(index)) > 0;
 	}
-	this.flip = function(index){
-		this.value ^= Math.pow(2, index);
+	set(index){	//set bit at index to 1 regardless of current value
+		this.value |= this.bit(index);
 	}
-	this.log2 = function(){
-		return this.value === 0 || Math.log2(this.value) === parseInt(Math.log2(this.value));
+	reset(index){	//set bit at index to 0 regardless of current value
+		this.value &= ~(this.bit(index));
+	}
+	flip(index){	//flip bit at index. 1->0, 0->1
+		this.value ^= this.bit(index);
+	}
+	log2(){	//true if value is 0 or value is a power of 2
+		let log = Math.log2(this.value);
+		return this.value === 0 || log === parseInt(log);
 	}
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 let Global = {
-  c: document.getElementById("c"),
-  ctx: c.getContext("2d"),
-  fps: 60,
-  keys: new BitSet(0),
-  tilesize: 40,
+  c: document.getElementById("c"),  //canvas
+  ctx: c.getContext("2d"),  //canvas draw context
+  fps: 60,  //frames per second
+  keys: new BitSet(0),  //keeps track of which keys are pressed
+  tilesize: 40, //width and height of each tile
 
-  Key: {
+  Key: {  //indices for keys
     w: 3,
     a: 2,
     s: 1,
     d: 0
   },
-  Colors: [
+  Colors: [ //possible tile colors
     "#aa0000",
     "#00aa00",
     "#0000aa",
@@ -19,35 +19,45 @@ let Global = {
     "#00aaaa"
   ],
 
-  rect: function(x, y, w, h, color){
+  rect: function(x, y, w, h, color){  //draw a filled rectangle at (x, y)
     Global.ctx.fillStyle = color;
     Global.ctx.fillRect(x, y, w, h);
   },
-  clamp: function(x, min, max){
+  clamp: function(x, min, max){ //clamps x to [min, max]
     return Math.min(max, Math.max(x, min));
   }
 }
-Global.c.width = Global.tilesize * 15;
-Global.c.height = Global.tilesize * 15;
-Global.c.style = "border: 1px solid black;";
+Global.c.width = Global.tilesize * 15;  //canvas is 15 tiles wide
+Global.c.height = Global.tilesize * 15; //canvas is 15 tiles tall
+Global.c.style = "border: 1px solid black;";  //black border around canvas
 
-function grid(w, h){
+function grid(w, h){  //create grid of tiles with dimensions wxh
   var arr = [];
-
-  for(var i = 0; i < h; i++){
-    arr.push([]);
-    for(var j = 0; j < w; j++){
-      arr[i].push(parseInt(Math.random() * (Global.Colors.length)));//i % 2 + j % 2 != 1 ? 0 : 1);
+  for(var i = 0; i < h; i++){ //for each row
+    arr.push([]); //create new row
+    for(var j = 0; j < w; j++){ //for each column
+      arr[i].push(parseInt(Math.random() * (Global.Colors.length)));  //create new tile with random color
     }
   }
-
   return arr;
 }
-let pos = {x: 7, y: 7}, range = 7;
-let g = grid(100, 100);
+let pos = {x: 7, y: 7}, range = 7;  //starting position of player, radius of renderer
+let g = grid(100, 100); //create new grid
 
 function render(g){
   let size = Global.tilesize;
+  //radial occlusion culling algorithm
+  //using equation: x^2 + y^2 = r^2
+  //
+  //begin on left side of circle; that is, x = -radius
+  //iterate through until right side is reached, or x = radius
+  //
+  //for each iteration, calculate y boundary: y = sqrt(r^2 - x^2)
+  //loop through from top to bottom; from y to -y
+  //
+  //clamp current position to grid
+  //if current position is player position, draw pink rectangle
+  //otherwise, draw rectangle from the grid
   for(var x = -range; x <= range; x++){
     let y = parseInt((Math.sqrt(range * range - x * x + 1)));
     for(var i = y; i >= -y; i--){
@@ -63,20 +73,23 @@ function render(g){
 }
 
 
-function update(){
-  Global.ctx.clearRect(0, 0, Global.c.width, Global.c.height);
+function update(){  //game loop
+  Global.ctx.clearRect(0, 0, Global.c.width, Global.c.height);  //clear sceen
 
-  render(g);
+  render(g);  //draw grid
 
-  pos.y += (Global.keys.at(Global.Key.s) - Global.keys.at(Global.Key.w)) * 1;//Global.clamp(pos.y - 1, 0, g.length - 1);
+  //update player position
+  pos.y += (Global.keys.at(Global.Key.s) - Global.keys.at(Global.Key.w)) * 1;
   pos.x += (Global.keys.at(Global.Key.d) - Global.keys.at(Global.Key.a)) * 1;
+  //clamp player position to grid
   pos.y = Global.clamp(pos.y, 0, g.length - 1);
   pos.x = Global.clamp(pos.x, 0, g[pos.y].length - 1);
 
-  setTimeout(update, 1000 / Global.fps);
+  setTimeout(update, 1000 / Global.fps);  //wait for next frame
 }
 update();
 
+//handle keyboard input and update Global.keys
 window.onkeydown = function(e){
   switch(e.keyCode){
   case 87: Global.keys.set(Global.Key.w); break;
@@ -92,9 +105,4 @@ window.onkeyup = function(e){
   case 83: Global.keys.reset(Global.Key.s); break;
   case 68: Global.keys.reset(Global.Key.d); break;
   }
-}
-
-
-function dijkstras(){
-
 }
